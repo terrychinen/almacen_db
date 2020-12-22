30cd5fd7095731a456261ea1df38ba7def1e69d8
Tery <terrychinen55@gmail.com>
2020-12-22 01:43:42 -0500

    Agregando la tabla person_email, person_phone y person_address

--- almacen.sql
+++ almacen.sql
@@ -14,3 +14,42 @@ CREATE TABLE person(
     photo                    VARCHAR(200)       NULL DEFAULT '',		 					 /*foto*/
     state                    TINYINT       	    NOT NULL DEFAULT 1		 					 /*estado*/
 );
+
+
+
+
+/*PERSONA_EMAIL*/
+CREATE TABLE person_email(
+    person_email_id          INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,		/*persona_correo_electronico_id*/
+    person_id                BIGINT          NOT NULL,									/*persona_id*/
+    email                    VARCHAR(200)    NOT NULL UNIQUE,							/*correo_electronica*/
+    state                    TINYINT       	 NOT NULL DEFAULT 1,						/*estado*/
+
+    FOREIGN KEY     (person_id)     REFERENCES person(person_id)
+);
+
+
+
+
+/*PERSONA_TELEFONO*/
+CREATE TABLE person_phone(
+    person_phone_id          INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,		/*persona_telefono_id*/
+    person_id                BIGINT          NOT NULL,									/*persona_id*/
+    phone                    VARCHAR(200)    NOT NULL UNIQUE,							/*telefono*/          
+    state                    TINYINT       	 NOT NULL DEFAULT 1,						/*estado*/
+
+    FOREIGN KEY     (person_id)     REFERENCES person(person_id)
+);
+
+
+
+
+/*PERSONA_DIRECCION*/
+CREATE TABLE person_address(
+    person_address_id        INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,		/*persona_direccion_id*/
+    person_id                BIGINT          NOT NULL,									/*persona_id*/
+    address                  VARCHAR(200)    NOT NULL UNIQUE,							/*direccion*/
+    state                    TINYINT       	 NOT NULL DEFAULT 1,						/*estado*/
+
+    FOREIGN KEY     (person_id)     REFERENCES person(person_id)
+);
